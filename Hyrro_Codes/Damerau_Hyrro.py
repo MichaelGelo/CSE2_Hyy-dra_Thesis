35eff426e7521fc89fcf8c684c9369e435e2e4e4
MichaelGelo <101308741+MichaelGelo@users.noreply.github.com>
2025-10-15 10:53:32 +0800

    added final resources

--- Hyrro_Codes/Damerau_Hyrro.py
+++ Hyrro_Codes/Damerau_Hyrro.py
@@ -139,8 +139,8 @@ print ("Generate Damerau local edit distance score of pattern of length m matchi
 #t = "GTAGGCTTAAGGTTA"
 #p = "JAAAT"
 #t = "TAAAJ"
-p="AGTC"        #query
-t="AAAATGCA"    #reference
+p="ACGTACGTACGT"        #query
+t="ACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGT"    #reference
 #p = 'MATCH'
 #t = 'REMACHINE' 
 
---
+++ Hyrro_Codes/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+for (int i = 0; i < n; i++) {
+            //     printf("%d", scores[i]);
+            //     if (i < n - 1) printf(",");
+            // }
