37c4719f8e4b16bc441130cc77d366bf67ca0296
MichaelGelo <101308741+MichaelGelo@users.noreply.github.com>
2025-11-19 23:16:46 +0800

    Delete FPGA-GPU directory

--- FPGA-GPU/fpga.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Run Python script to send files and execute on FPGA
-python "E:\FPGA-GPU\fpga_main.py"
-pause
--- FPGA-GPU/fpga_multiple.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Run Python script to send files and execute on FPGA
-python "E:\FPGA-GPU\multiple_fpga_main.py"
-pause
